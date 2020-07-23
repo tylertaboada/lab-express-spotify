0b37dc3521524b4d4a783c026f3363c0c6ea9306
Tyler Taboada <tjy5008@gmail.com>
2020-07-23 14:12:18 +0100

    not finished

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -13,6 +15,55 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error =>
+    console.log('Something went wrong when retrieving an access token', error)
+  );
+
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/artist-search-result', (req, res) => {
+  const term = req.query.term;
+  console.log(term);
+  spotifyApi
+    .searchArtists(term)
+    .then(data => {
+      console.log('The received data from the API: ', data.body.artists.items);
+      const artists = data.body.artists.items;
+      res.render('artist-search-result', { artists });
+    })
+    .catch(err =>
+      console.log('The error while searching artists occurred: ', err)
+    );
+});
+
+// app.get('/artist-search-result/:id', (req, res) => {
+//   const id = req.params.id;
+//   console.log(id);
+//   spotifyApi.getArtistAlbums(id).then(
+//     function (data) {
+//       console.log('Artist albums', data.body);
+//       res.render('/artist-search-result');
+//     },
+//     function (err) {
+//       console.error(err);
+//     }
+//   );
+// });
+
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+);
